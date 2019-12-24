72aa134f22871a05e5e8e69aeafd2c1fa65498ff
20181106279 <1771478860@qq.com>
2019-12-24 14:40:24 +0800

    Initial Commit

---
+++ lxyardinuo/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  lxyardinuo
+//
+//  Created by 20181106279 on 2019/12/24.
+//  Copyright © 2019 20181106279. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
